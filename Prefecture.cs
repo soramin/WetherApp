6c6a97029b70688967d8a8b67b8595c81df54828
soramin <44796681+soramin@users.noreply.github.com>
2020-05-12 19:17:42 +0900

    Add files via upload

---
+++ Prefecture.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.Serialization;
+
+namespace WetherApp.Models
+{
+    //都道府県管理クラス
+    [DataContract]
+    class Prefecture
+    {
+        //都道府県名
+        [DataMember]
+        public string Name { get; set; }
+
+        //都道府県に対応する市町村メンバー
+        [DataMember]
+        public List<City> Cities { get; set; }
+    }
+
+    //市町村管理クラス
+    [DataContract]
+    class City
+    {
+        // <summary>
+        // 市町村ID
+        // </summary>
+        [DataMember]
+        public string Id { get; set; }
+
+        //市町村名
+        [DataMember]
+        public string Name { get; set; }
+    }
+}
